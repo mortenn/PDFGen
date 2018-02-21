c8b79a6d4407f10c439a71f2e7d7253edb741498
Morten Nilsen <morten@runsafe.no>
2018-02-21 19:52:04 +0100

    Reorganize controller

--- app/generator.js
+++ app/generator.js
@@ -9,26 +9,27 @@ angular.module('pdfgen').component(
 function Generator(API, $scope)
 {
 	var reader = new FileReader();
+	var ctrl = this;
+	reader.onload = function(){ $scope.$apply(function(){ ctrl.loadedCSV(); });	};
+
 	this.loading = false;
 	this.csv = false;
-	reader.onload = function()
-	{
-		$scope.$apply(
-			function()
-			{
-				ctrl.loading = false;
-				ctrl.csv = reader.result;
-				ctrl.columns = ctrl.csv.split(/[\n\r]/)[0].split(';');
-			}
-		);
-	};
+	this.format = '';
+	this.previewImage = false;
+
 	this.loadCSV = function(file)
 	{
 		this.loading = true;
 		reader.readAsText(file);
 	};
-	this.format = '';
-	this.previewImage = false;
+
+	this.loadedCSV = function()
+	{
+		ctrl.loading = false;
+		ctrl.csv = reader.result;
+		ctrl.columns = ctrl.csv.split(/[\n\r]/)[0].split(';');
+	};
+
 	this.preview = function()
 	{
 		this.previewImage = API.GetPreview({format:this.format,data:{}});
